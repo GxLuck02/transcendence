05aacbbf789a97c87685c740ef822524db85c992
GxLuck02 <ttreichl@student.42lausanne.ch>
2025-11-17 16:04:46 +0000

    premier test pong

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import PongGame from './components/PongGame'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PONG GAME</h1>
+      <PongGame />
+    </div>
+  )
+}
+
+export default App
